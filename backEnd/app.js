b376cd06f24453652abe01d8d1a51956a4850c1d
adilhusain01 <husainadil202@gmail.com>
2024-02-10 00:48:39 +0530

    main app

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,2 +1,13 @@
 const express = require("express");
+const router = require(("./routes/taskRoutes"));
 
+const app = express();
+
+//takes req.body
+app.use(express.json());
+
+app.use('/tasks', router);
+
+app.listen(8000, (req, res) => {
+    console.log('Server running on port 8000');
+})
