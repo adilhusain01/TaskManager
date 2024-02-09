706cff2f55831dfc36a2a92b42650e074f5694df
adilhusain01 <husainadil202@gmail.com>
2024-02-10 01:47:02 +0530

    main app

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = require(("./routes/taskRoutes"));
 
 const app = express();
@@ -6,8 +7,14 @@ const app = express();
 //takes req.body
 app.use(express.json());
 
+mongoose.connect("mongodb+srv://adil:Nigar01@cluster0.hgsaitb.mongodb.net/").then(() => {
+    console.log("Connected to mongoDB");
+}).catch((err) => {
+    console.log("Error connecting to mongoDB", err);
+})
+
 app.use('/tasks', router);
 
-app.listen(8000, (req, res) => {
+app.listen(8000, () => {
     console.log('Server running on port 8000');
 })
