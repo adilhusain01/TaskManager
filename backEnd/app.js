f40fe38e9303df793f5e8e82f4d7914905000cc9
adilhusain01 <husainadil202@gmail.com>
2024-02-10 11:18:04 +0530

    main app

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require(("./routes/taskRoutes"));
+const router = require(("./src/routes/taskRoutes"));
 
 const app = express();
 
